e1ead39eab2f59fa42343d5d44abea3b32e8dc18
Greg <melozy@free.fr>
2024-09-13 13:28:03 +0200

    mise à jour onclick pour bouton modifier

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -38,7 +38,7 @@ export default function Card({ titre, frequence, id, suppr, tache, update, patch
           </div>
           <p className="is-size-4 px-4">{frequence}</p>
           <div className="bouton update">
-            <button className="button is-white is-outlined" onClick={() => update(tache) && setIsClicked(isClicked)}>Modifier</button>
+            <button className="button is-white is-outlined" onClick={() => update(tache) && handleEtatChange(tache._id, tache.etat)}>Modifier</button>
           </div>
         </div>
       </div>
