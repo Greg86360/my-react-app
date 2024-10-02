9c64ea438b90a4187c0e5c20b2c690620ca93216
Greg <melozy@free.fr>
2024-10-02 11:33:39 +0200

    mise à jour route accueil

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
     <>
       <Header />
       <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/" element={<Login />} />
       <Route path="/tasks" element={<Content />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
