61ec70d8472fa10b747415a5a6cc566852e15a8e
Greg <melozy@free.fr>
2024-10-02 09:53:44 +0200

    application terminée

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -12,35 +12,27 @@ export default function Header() {
     navigate('/login');
   };
 
-  const toggleMenu = () => {
-    setIsActive(!isActive); // Inverser l'état du menu
-  };
 
   return (
-    <nav className="navbar has-background-primary has-text-white is-align-items-center p-3" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
+    <nav className="navbar has-background-primary has-text-white is-align-items-center" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand is-flex is-justify-content-center	">
         <span className="navbar-item has-text-weight-bold is-uppercase has-text-white has-text-centered-touch	is-size-1 is-size-2-touch			">Ma super ToDoList</span>
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
+
       </div>
-      <div className={`navbar-menu ${isActive ? 'is-active' : ''}`}>
-        <div className="navbar-end">
-          {token ? (
+      <div className="navbar-end">
+        {token ? (
+          <div className="navbar-item">
             <button onClick={handleLogout} className="button is-rounded is-outlined">Se déconnecter</button>
-          ) : (
-            <div className={`navbar-item ${isActive ? 'is-active' : ''}`}>
+          </div>
+        ) : (
+          <div className="navbar-item">
 
-              <Link to="/register" className="button is-rounded is-outlined">S'inscrire</Link>
-              <Link to="/login" className="button is-rounded">Se connecter</Link>
-            </div>
+            <Link to="/register" className="button is-rounded is-outlined">S'inscrire</Link>
+            <Link to="/login" className="button is-rounded">Se connecter</Link>
+          </div>
 
-          )}
+        )}
 
-        </div>
       </div>
     </nav>
   );
